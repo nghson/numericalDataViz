d72340f1226dc365315557e3e5472a1b821e11f4
Son Nguyen <son.hngy@gmail.com>
2022-05-03 17:55:17 +0300

    Format for Point

--- src/main/scala/DataViz/Point.scala
+++ src/main/scala/DataViz/Point.scala
@@ -2,5 +2,5 @@ package DataViz
 
 class Point(val x: Double, val y: Double) {
 
-  override def toString: String = "(" + x + ", " + y + ")"
+  override def toString: String = "(" + Chart.fmt(x) + ", " + Chart.fmt(y) + ")"
 }
