1178cabc6375fb3ef2a7227948e6e9c1ab3173e5
Son Nguyen <son.hngy@gmail.com>
2022-05-03 17:54:39 +0300

    Grid now aligns with axes

--- src/main/scala/DataViz/Grid.scala
+++ src/main/scala/DataViz/Grid.scala
@@ -1,32 +1,38 @@
 package DataViz
 
-class Grid(val size: Double, val width: Double, val height: Double, val data: LineData) {
-  val verticals: Array[Array[Point]] = {
+object Grid {
+  def verticals(size: Double, width: Double, height: Double, data: LineData, x: Double): Array[Array[Point]] = {
     val arr = scala.collection.mutable.ArrayBuffer[Array[Point]]()
-    val dimensions: (Double, Double) = {
-      if (data.dimensions._1 > 0) data.dimensions
-      else (100, data.dimensions._2)
-    }
+    val dimensions: (Double, Double) = data.dimensions
     val scaleX = width / dimensions._1
-    var current = size
-    while (current <= dimensions._1) {
-      arr += Array(new Point(current * scaleX, 0), new Point(current * scaleX, height))
-      current += size
+    val scaledSize = size * scaleX
+    var current = x + scaledSize
+    while (current <= width) {
+      arr += Array(new Point(current, 0), new Point(current, height))
+      current += scaledSize
+    }
+    current = x - scaledSize
+    while (current >= 0) {
+      arr += Array(new Point(current, 0), new Point(current, height))
+      current -= scaledSize
     }
     arr.toArray
   }
 
-  val horizontals: Array[Array[Point]] = {
+  def horizontals(size: Double, width: Double, height: Double, data: LineData, y: Double): Array[Array[Point]] = {
     val arr = scala.collection.mutable.ArrayBuffer[Array[Point]]()
-    val dimensions: (Double, Double) = {
-      if (data.dimensions._2 > 0) data.dimensions
-      else (data.dimensions._1, 100)
-    }
+    val dimensions: (Double, Double) = data.dimensions
     val scaleY = height / dimensions._2
-    var current = size
+    val scaledSize = size * scaleY
+    var current = y + scaledSize
     while (current <= dimensions._2) {
-      arr += Array(new Point(0, current * scaleY), new Point(width, current * scaleY))
-      current += size
+      arr += Array(new Point(0, current), new Point(width, current))
+      current += scaledSize
+    }
+    current = y - scaledSize
+    while (current >= 0) {
+      arr += Array(new Point(0, current), new Point(width, current))
+      current -= scaledSize
     }
     arr.toArray
   }
