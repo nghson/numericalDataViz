7cc62f52b4cbf3b143f96d220c821a81229b12b8
Son Nguyen <son.hngy@gmail.com>
2022-05-04 07:13:20 +0300

    Simplify Grid and Axis object

--- src/main/scala/DataViz/Axis.scala
+++ src/main/scala/DataViz/Axis.scala
@@ -27,9 +27,13 @@ object Axis {
     }
   }
 
-  // return the marks on axis
+  /** Calculate the positions of the marks on x-axis.
+   *  @param y the y-coordinate of the x-axis.
+   */
   def get_x_marks(data: LineData, width: Double, height: Double, y: Double): (Array[Array[Point]], Array[Array[Double]]) = {
+    // coordinates of the marks so we need two points
     val arr = scala.collection.mutable.ArrayBuffer[Array[Point]]()
+    //coordinates of the number label so only need one point
     val arr2 = scala.collection.mutable.ArrayBuffer[Array[Double]]()
 
     val yLength = { // length of the mark
@@ -37,59 +41,47 @@ object Axis {
       else y - 5
     }
     val yLabel = { // y position for the mark labels
-      if (y == 0) y + 8
-      else y - 8
+      if (y == 0) y + 15
+      else y - 15
     }
 
     val scaleX = width / data.dimensions._1
     var unit = pow(10, floor(log10(data.dimensions._1)))
     if (data.dimensions._1 <= unit * 2) unit = unit/4
     else if (data.dimensions._1 <= unit * 5) unit = unit/2
+    else if (data.dimensions._1 >= unit * 7) unit = unit*2
 
     var current = unit
-    if (data.xRange._1 >= 0) {
-      while (current <= data.xRange._2) {
-        if (current >= data.xRange._1) {
-          arr +=
-            Array(new Point((current - data.xRange._1) * scaleX, y), new Point((current - data.xRange._1) * scaleX, yLength))
-          arr2 +=
-            Array(current, (current - data.xRange._1) * scaleX, yLabel)
-        }
-        current += unit
-      }
-    } else if (data.xRange._2 < 0) {
-      while (current >= data.xRange._1) {
-        if (current <= data.xRange._2) {
-          arr +=
-            Array(new Point((current - data.xRange._1) * scaleX, y), new Point((current - data.xRange._1) * scaleX, yLength))
-          arr2 +=
-            Array(current, (current - data.xRange._1) * scaleX, yLabel)
-        }
-        current -= unit
-      }
-    } else {
-      while (current <= data.xRange._2) {
+    while (current <= data.xRange._2) {
+      if (current >= data.xRange._1) {
         arr +=
           Array(new Point((current - data.xRange._1) * scaleX, y), new Point((current - data.xRange._1) * scaleX, yLength))
         arr2 +=
           Array(current, (current - data.xRange._1) * scaleX, yLabel)
-        current += unit
       }
-      current = -unit
-      while (current >= data.xRange._1) {
+      current += unit
+    }
+    current = -unit
+    while (current >= data.xRange._1) {
+      if (current <= data.xRange._2) {
         arr +=
           Array(new Point((current - data.xRange._1) * scaleX, y), new Point((current - data.xRange._1) * scaleX, yLength))
         arr2 +=
           Array(current, (current - data.xRange._1) * scaleX, yLabel)
-        current -= unit
       }
+      current -= unit
     }
 
     (arr.toArray, arr2.toArray)
   }
 
+  /** Calculate the positions of the marks on y-axis.
+   *  @param x the x-coordinate of the y-axis.
+   */
   def get_y_marks(data: LineData, width: Double, height: Double, x: Double): (Array[Array[Point]], Array[Array[Double]]) = {
+    // coordinates of the marks so we need two points
     val arr = scala.collection.mutable.ArrayBuffer[Array[Point]]()
+    //coordinates of the number label so only need one point
     val arr2 = scala.collection.mutable.ArrayBuffer[Array[Double]]()
 
     val xLength = { // length of the mark
@@ -98,51 +90,34 @@ object Axis {
     }
     val xLabel = { // x position for the mark labels
       if (x >= width - 50) x - 25
-      else x + 8
+      else x + 15
     }
 
     val scaleY = height / data.dimensions._2
     var unit = pow(10, floor(log10(data.dimensions._2)))
     if (data.dimensions._2 <= unit * 2) unit = unit/4
     else if (data.dimensions._2 <= unit * 5) unit = unit/2
+    else if (data.dimensions._2 >= unit * 7) unit = unit * 2
 
     var current = unit
-    if (data.yRange._1 >= 0) {
-      while (current <= data.yRange._2) {
-        if (current >= data.yRange._1) {
-          arr +=
-            Array(new Point(x, (-current+data.yRange._2)*scaleY), new Point(xLength, (-current + data.yRange._2) * scaleY))
-          arr2 +=
-            Array(current, xLabel, (-current + data.yRange._2) * scaleY)
-        }
-        current += unit
-      }
-    } else if (data.yRange._2 < 0) {
-      while (current >= data.yRange._1) {
-        if (current <= data.yRange._2) {
-          arr +=
-            Array(new Point(x, (-current+data.yRange._2)*scaleY), new Point(xLength, (-current + data.yRange._2) * scaleY))
-          arr2 +=
-            Array(current, xLabel, (-current + data.yRange._2) * scaleY)
-        }
-        current -= unit
-      }
-    } else {
-      while (current <= data.yRange._2) {
+    while (current <= data.yRange._2) {
+      if (current >= data.yRange._1) {
         arr +=
-          Array(new Point(x, (-current + data.yRange._2) * scaleY), new Point(xLength, (-current + data.yRange._2) * scaleY))
+          Array(new Point(x, (-current+data.yRange._2)*scaleY), new Point(xLength, (-current + data.yRange._2) * scaleY))
         arr2 +=
           Array(current, xLabel, (-current + data.yRange._2) * scaleY)
-        current += unit
       }
-      current = -unit
-      while (current >= data.yRange._1) {
+      current += unit
+    }
+    current = -unit
+    while (current >= data.yRange._1) {
+      if (current <= data.yRange._2) {
         arr +=
-          Array(new Point(x, (-current + data.yRange._2) * scaleY), new Point(xLength, (-current + data.yRange._2) * scaleY))
+          Array(new Point(x, (-current+data.yRange._2)*scaleY), new Point(xLength, (-current + data.yRange._2) * scaleY))
         arr2 +=
           Array(current, xLabel, (-current + data.yRange._2) * scaleY)
-        current -= unit
       }
+      current -= unit
     }
 
     (arr.toArray, arr2.toArray)
--- src/main/scala/DataViz/Grid.scala
+++ src/main/scala/DataViz/Grid.scala
@@ -1,39 +1,56 @@
 package DataViz
 
 object Grid {
+  /** Get vertical lines in the grid.
+   *  @param size grid size
+   *  @param x x-coordinate of the y-axis */
   def verticals(size: Double, width: Double, height: Double, data: LineData, x: Double): Array[Array[Point]] = {
     val arr = scala.collection.mutable.ArrayBuffer[Array[Point]]()
-    val dimensions: (Double, Double) = data.dimensions
-    val scaleX = width / dimensions._1
-    val scaledSize = size * scaleX
-    var current = x + scaledSize
-    while (current <= width) {
-      arr += Array(new Point(current, 0), new Point(current, height))
-      current += scaledSize
+    val scaleX = width / data.dimensions._1
+
+    var current = size
+    while (current <= data.xRange._2) {
+      if (current >= data.xRange._1) {
+        arr +=
+          Array(new Point((current - data.xRange._1) * scaleX, 0), new Point((current - data.xRange._1) * scaleX, height))
+      }
+      current += size
     }
-    current = x - scaledSize
-    while (current >= 0) {
-      arr += Array(new Point(current, 0), new Point(current, height))
-      current -= scaledSize
+    current = -size
+    while (current >= data.xRange._1) {
+      if (current <= data.xRange._2) {
+        arr +=
+          Array(new Point((current - data.xRange._1) * scaleX, 0), new Point((current - data.xRange._1) * scaleX, height))
+      }
+      current -= size
     }
+
     arr.toArray
   }
 
+  /** Get horizontal lines in the grid.
+   *  @param y y-coordinate of the x-axis */
   def horizontals(size: Double, width: Double, height: Double, data: LineData, y: Double): Array[Array[Point]] = {
     val arr = scala.collection.mutable.ArrayBuffer[Array[Point]]()
-    val dimensions: (Double, Double) = data.dimensions
-    val scaleY = height / dimensions._2
-    val scaledSize = size * scaleY
-    var current = y + scaledSize
-    while (current <= height) {
-      arr += Array(new Point(0, current), new Point(width, current))
-      current += scaledSize
+    val scaleY = height / data.dimensions._2
+
+    var current = size
+    while (current <= data.yRange._2) {
+      if (current >= data.yRange._1) {
+        arr +=
+          Array(new Point(0, (data.yRange._2 - current) * scaleY), new Point(width, (data.yRange._2 - current) * scaleY))
+      }
+      current += size
     }
-    current = y - scaledSize
-    while (current >= 0) {
-      arr += Array(new Point(0, current), new Point(width, current))
-      current -= scaledSize
+    current = -size
+    while (current >= data.yRange._1) {
+      if (current <= data.yRange._2) {
+        arr +=
+          Array(new Point(0, (data.yRange._2 - current) * scaleY), new Point(width, (data.yRange._2 - current) * scaleY))
+      }
+      current -= size
     }
+
     arr.toArray
   }
 }
